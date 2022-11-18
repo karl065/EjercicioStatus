2b9683189b6c9526a510c9753d2b2af9dd7f2de4
karl065 <karlos065@hotmail.com>
2022-11-18 14:08:54 -0500

    App de Status

---
+++ app/src/main/java/karlos/ejerciciostatus/MainActivity.java
@@ -0,0 +1,51 @@
+package karlos.ejerciciostatus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mat;
+    private EditText fis;
+    private EditText qui;
+    private TextView sta;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mat = (EditText) findViewById(R.id.txtMatematicas);
+        fis = (EditText) findViewById(R.id.txtFisica);
+        qui = (EditText) findViewById(R.id.txtQuimica);
+        sta = (TextView) findViewById(R.id.statusAlumn);
+
+
+
+    }
+
+    public void Status(View view){
+        String matematicasString = mat.getText().toString();
+        String fisicaString = fis.getText().toString();
+        String quiString = qui.getText().toString();
+
+        int matematicasInt = Integer.parseInt(matematicasString);
+        int fisicaInt = Integer.parseInt(fisicaString);
+        int quimicaInt = Integer.parseInt(quiString);
+
+        int promedio = (matematicasInt + fisicaInt + quimicaInt)/3;
+
+        if (promedio >= 6){
+            sta.setText("Estatus Aprovado con " + promedio);
+        }else if (promedio < 6){
+            sta.setText("Estatus Reprobado con " + promedio);
+        }
+
+    }
+
+}
