1a11972a48daeab019aa3ce3361e6b5fefaed747
karl065 <karlos065@hotmail.com>
2022-11-18 15:55:07 -0500

    Correcion de errores Status V001-02

--- app/src/main/java/karlos/ejerciciostatus/MainActivity.java
+++ app/src/main/java/karlos/ejerciciostatus/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText fis;
     private EditText qui;
     private TextView sta;
+    private Button boton;
 
 
     @Override
@@ -24,12 +26,20 @@ public class MainActivity extends AppCompatActivity {
         fis = (EditText) findViewById(R.id.txtFisica);
         qui = (EditText) findViewById(R.id.txtQuimica);
         sta = (TextView) findViewById(R.id.statusAlumn);
+        boton = (Button) findViewById(R.id.botonEvaluar);
 
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Status(sta);
 
-
+            }
+        });
     }
 
-    public void Status(View view){
+
+
+    public void Status(View view) {
         String matematicasString = mat.getText().toString();
         String fisicaString = fis.getText().toString();
         String quiString = qui.getText().toString();
@@ -38,13 +48,14 @@ public class MainActivity extends AppCompatActivity {
         int fisicaInt = Integer.parseInt(fisicaString);
         int quimicaInt = Integer.parseInt(quiString);
 
-        int promedio = (matematicasInt + fisicaInt + quimicaInt)/3;
+        int promedio = (matematicasInt + fisicaInt + quimicaInt) / 3;
 
-        if (promedio >= 6){
+        if (promedio >= 6) {
             sta.setText("Estatus Aprovado con " + promedio);
-        }else if (promedio < 6){
+        } else if (promedio < 6) {
             sta.setText("Estatus Reprobado con " + promedio);
         }
+
 
     }
 
